f4fb9e0e4f3322d8a58e0071c1d1c7a589913181
Kareem Fekry <kareem_91@outlook.com>
2021-09-14 12:43:59 +0200

    Register

--- SupportI/ViewControllers/ِAuth/RegisterViewController.swift
+++ SupportI/ViewControllers/ِAuth/RegisterViewController.swift
@@ -8,29 +8,30 @@
 
 import UIKit
 
-class RegisterViewController: UIViewController {
-   
+class RegisterViewController: BaseController {
+    
     @IBOutlet weak var userNameTxt: UITextField!
     @IBOutlet weak var emialOrPhoneTxt: UITextField!
     @IBOutlet weak var passwordTxt: UITextField!
-
+    
     
     var viewModel : Auth1ViewModel?
     var parameters : [String : Any] = [:]
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setup()
+        bind()
     }
-    
-    
     @IBAction func loginClicked(_ sender: UIButton) {
+        let scene = self.controller(LoginController.self,storyboard: .auth)
+        self.push(scene)
     }
     @IBAction func signUpClicked(_ sender: UIButton) {
-        signUp()
+        if (validateTextFields()) {
+            signUp()
+        }
     }
-
     @IBAction func signUpWithAppleClicked(_ sender: UIButton) {
     }
     @IBAction func signUpWithGoogleClicked(_ sender: UIButton) {
@@ -38,11 +39,20 @@ class RegisterViewController: UIViewController {
     
     @IBAction func signUpWithFacebookClicked(_ sender: UIButton) {
     }
-
-    func signUp() {
-        
+    func validateTextFields() -> Bool {
         emialOrPhoneTxt.customValidationRules = [RequiredRule() , EmailRule()]
+        userNameTxt.customValidationRules = [RequiredRule()]
         passwordTxt.customValidationRules = [RequiredRule(), MinLengthRule(length: 6)]
+        let validator = Validation(textFields: [emialOrPhoneTxt,passwordTxt])
+        return validator.success
+    }
+    
+    func setup() {
+       viewModel = .init()
+       viewModel?.delegate = self
+   }
+    
+    func signUp() {
         parameters["username"] = userNameTxt.text ?? ""
         parameters["email"] = emialOrPhoneTxt.text ?? ""
         parameters["phone"] = emialOrPhoneTxt.text ?? ""
@@ -51,11 +61,11 @@ class RegisterViewController: UIViewController {
         parameters["grant_type"] = "password"
         viewModel?.RegisterApi(paramters: parameters)
     }
-    func bind() {
-       viewModel?.userdata.bind({ [weak self](data) in
-           self?.stopLoading()
-           print(data)
-       })
-   }
+    override func bind() {
+        viewModel?.userdata.bind({ [weak self](data) in
+            self?.stopLoading()
+            print(data)
+        })
+    }
     
 }
