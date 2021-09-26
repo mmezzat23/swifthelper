6582d727e08900e702d8875887369af43cca36df
Kareem <kareem_91@outlook.com>
2021-09-26 18:54:43 +0200

    Settings

---
+++ SupportI/ViewControllers/Setting/mapViewController.swift
@@ -0,0 +1,27 @@
+//
+//  mapViewController.swift
+//  SupportI
+//
+//  Created by Kareem on 9/26/21.
+//  Copyright © 2021 MohamedAbdu. All rights reserved.
+//
+
+import UIKit
+import GoogleMaps
+
+class mapViewController: UIViewController {
+
+    @IBOutlet weak var mapView: GMSMapView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func yesClicked(_ sender: UIButton) {
+    }
+    
+    @IBAction func discardClicked(_ sender: UIButton) {
+    }
+    
+}
