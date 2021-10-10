452452f6156c008c94535608093c236aff431760
adam <mmezzat23@gmail.com>
2021-10-10 16:43:09 +0200

    sprint 2 changes

---
+++ SupportI/ViewControllers/Profile/Editemail.swift
@@ -0,0 +1,30 @@
+//
+//  Editemail.swift
+//  Wndo
+//
+//  Created by Adam on 10/10/2021.
+//  Copyright © 2021 MohamedAbdu. All rights reserved.
+//
+
+import UIKit
+
+class Editemail: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
