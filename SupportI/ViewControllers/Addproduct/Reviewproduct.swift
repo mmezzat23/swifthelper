df15cf1df5a0f3540bdf5f7364ac15d063e6db2f
adam <mmezzat23@gmail.com>
2021-10-19 17:03:59 +0200

    Sprint 3 design review

--- SupportI/ViewControllers/Addproduct/Reviewproduct.swift
+++ SupportI/ViewControllers/Addproduct/Reviewproduct.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import BMPlayer
 
 class Reviewproduct: BaseController {
     @IBOutlet weak var taxes: UILabel!
@@ -16,23 +17,46 @@ class Reviewproduct: BaseController {
     @IBOutlet weak var pricelbl: UILabel!
     @IBOutlet weak var buyerprice: UILabel!
     @IBOutlet weak var pricesummary: UILabel!
-    @IBOutlet weak var discount: UITextField!
-    @IBOutlet weak var pricetxt: UITextField!
+    @IBOutlet weak var productname: UILabel!
+    @IBOutlet weak var productdep: UILabel!
+    @IBOutlet weak var productdes: UILabel!
+    @IBOutlet weak var player: BMPlayer!
+    @IBOutlet weak var photos: UICollectionView!
+    @IBOutlet weak var productinfo: UITableView!
+    
+    @IBOutlet weak var dateview: UIView!
+    @IBOutlet weak var datehight: NSLayoutConstraint!
+    @IBOutlet weak var datelbl: UILabel!
+    @IBOutlet weak var pricetxtlbl: UILabel!
+    @IBOutlet weak var discountlbl: UILabel!
+    @IBOutlet weak var picktimelbl: UILabel!
+    @IBOutlet weak var timelbl: UILabel!
+    @IBOutlet weak var addresslbl: UILabel!
+    @IBOutlet weak var delivarylbl: UILabel!
+    @IBOutlet weak var sizeview: UIView!
+    @IBOutlet weak var sizehight: NSLayoutConstraint!
+    @IBOutlet weak var sizes: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        hiddenNav = true
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func editprice(_ sender: Any) {
     }
-    */
-
+    @IBAction func editship(_ sender: Any) {
+    }
+    @IBAction func editinfo(_ sender: Any) {
+    }
+    @IBAction func editmedia(_ sender: Any) {
+    }
+    @IBAction func editname(_ sender: Any) {
+    }
+    
+    @IBAction func edit(_ sender: Any) {
+    }
+    @IBAction func post(_ sender: Any) {
+    }
+    
 }
